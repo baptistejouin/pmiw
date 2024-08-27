288c6575dd373ce0b36254667468bd00fa277e28
baptistejouin <baptiste.jouin44@gmail.com>
2024-08-27 19:40:48 -0300

    fix mouse and enhance gradient

--- tp1/sketch.js
+++ tp1/sketch.js
@@ -1,81 +1,80 @@
+const gridSize = 300;
+const radius = 200;
+let rightBaseColor;
+
 function setup() {
-  // Two-dimensional canvas, left is for before, right is for after
   createCanvas(800, 400);
   noStroke();
+
+  rightBaseColor = color(random(255), random(255), random(255));
 }
 
-function drawGrid(cols, rows, gridSize, baseColor) {
+function drawGrid(cols, rows, baseColor) {
   for (let i = 1; i < cols; i++) {
     baseColor.setGreen((255 * i) / cols);
     for (let j = 1; j < rows; j++) {
       baseColor.setBlue((255 * j) / rows);
-      // Calculate the position of the point in the grid
+
       const x = (i * gridSize) / cols;
       const y = (j * gridSize) / rows;
 
-      // Set the fill color before drawing the ellipse
       fill(baseColor);
-      // Draw the point, map size with position in the grid
-      const minSize = 5;
-      const maxSize = 20;
 
-      const sizeX = map(x, 0, gridSize, minSize, maxSize);
-      const sizeY = map(y, 0, gridSize, minSize, maxSize);
+      const size = map(x, 0, gridSize, 5, 20);
 
-      ellipse(x, y, sizeX, sizeY);
+      ellipse(x, y, size, size);
     }
   }
 }
 
-function drawReactive(cols, rows, gridSize, baseColor) {
+function drawReactive(cols, rows, baseColor) {
   for (let i = 1; i < cols; i++) {
     baseColor.setGreen((255 * i) / cols);
     for (let j = 1; j < rows; j++) {
       baseColor.setBlue((255 * j) / rows);
-      // Calculate the position of the point in the grid
+
       const x = (i * gridSize) / cols;
       const y = (j * gridSize) / rows;
 
-      // Set the fill color before drawing the ellipse
       fill(baseColor);
-      // Draw the point, map size with position in the grid
+
       const minSize = 5;
       const maxSize = 20;
 
-      // map mouseX, change the size of the ellipse
-      const mappingX = map(mouseX, 0, width, 0, gridSize);
-      const mappingY = map(mouseY, 0, height, 0, gridSize);
+      const distance = dist(x, y, mouseX - 420, mouseY); // adjust mouse coordinates for the right grid
 
-      // Map the distance to the size of the ellipse
-      let sizeX = map(mappingX, 0, gridSize, minSize, maxSize);
-      let sizeY = map(mappingY, 0, gridSize, minSize, maxSize);
+      let size = minSize;
+      if (distance < radius) {
+        const growthFactor = map(distance, 0, radius, 20, 5);
+        size = growthFactor;
+      }
 
-      // Placement in the grid influence the size
-      const distance = dist(x, y, mappingX, mappingY);
-      const maxSizeDistance = dist(0, 0, gridSize, gridSize);
-      const sizeDistance = map(distance, 0, maxSizeDistance, maxSize, minSize);
-
-      // The final size is the minimum of the two
-      sizeX = min(sizeX, sizeDistance);
-      sizeY = min(sizeY, sizeDistance);
-
-      ellipse(x, y, sizeX, sizeY);
+      ellipse(x, y, size, size);
     }
   }
+}
+
+function randomBaseColor() {
+  let hue = random(360);
+  let saturation = random(60, 100); // keep saturation high for vibrant colors
+  let brightness = random(70, 100); // keep brightness high for a lighter (more pleasant gradient)
+  return color(hue, saturation, brightness);
 }
 
 function draw() {
-  const gridSize = 300;
-
   background(220);
   fill(0);
-  text(`${mouseX}, ${mouseY}`, 10, 10);
+  text(`Mouse coords: x:${mouseX}, y:${mouseY}`, 10, 10);
 
-  // First canvas
+  // first grid
   translate(20, 20);
-  drawGrid(15, 15, gridSize, color(255, 0, 0));
+  drawGrid(15, 15, color(140, 60, 10));
 
-  // Move to the second canvas, draw the reactive
-  translate(400 + 20, 0);
-  drawReactive(15, 15, gridSize, color(0, 0, 255));
+  // second grid
+  translate(400, 0);
+  drawReactive(15, 15, rightBaseColor);
+}
+
+function mousePressed() {
+  rightBaseColor = randomBaseColor();
 }
